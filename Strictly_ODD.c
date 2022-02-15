93ecff5afd174ab169c62f3fe62d89223ba8bf6b
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-02-15 21:41:25 +0530

    #include<stdio.h> int main() {    int i,n,r=0;    scanf("%d",&n);    int a[n];    for(i=0;i<n;i++)    {         scanf("%d",&a[i]);    }    for(i=0;i<n;i++)    {        if(a[i]%2!=0)        {            if(i%2!=0)            {              r++;            }            else            {                r=0;                break;            }        }    }    if(r!=0)    {        printf("True");    }    else    {        printf("False");    } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+   int i,n,r=0;
+   scanf("%d",&n);
+   int a[n];
+   for(i=0;i<n;i++)
+   {
+        scanf("%d",&a[i]);  
+   }
+   for(i=0;i<n;i++)
+   {
+       if(a[i]%2!=0)
+       {
+           if(i%2!=0)
+           {
+             r++;
+           }
+           else
+           {
+               r=0;
+               break;
+           }
+       }
+   }
+   if(r!=0)
+   {
+       printf("True");
+   }
+   else
+   {
+       printf("False");
+   }
+}
