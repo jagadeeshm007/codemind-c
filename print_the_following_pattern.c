d1ccd8324e59eb4440319a829ac127d4550b003c
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-03-08 11:05:37 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(i==j)             printf("0");             if(i!=j)             printf("x");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,18 @@
-  #include<stdio.h>
+#include<stdio.h>
 int main()
 {
-  int n,i=0,j;
-  char k,c[26];
-  for(k=65;k<=90;k++)
-  {
-  	c[i]= k;
-  	i++;
-  }
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-  {
-  	for(j=0;j<n;j++)
-  	{
-  		printf("%c ",c[i]);
-	  }
-	  printf("
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==j)
+            printf("0");
+            if(i!=j)
+            printf("x");
+        }
+        printf("
 ");
-  }
+    }
 }
