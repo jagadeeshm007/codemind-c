16f870ab6150c0715da5b47b967b28f67ef633d3
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-04-23 15:59:16 +0530

    #include<stdio.h>
    int main()
    {
        int n,i,j,c=0,k=0;
        scanf("%d",&n);
        int arr[n];
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
        }
        for(i=0;i<n;i++)
        {
    	     for(j=0;j<n;j++)
    	   	{
    	       if(arr[i]==arr[j]&&i!=j)
    	       {
    	          arr[j]=-1;
    	       }
    	    }
    	    if(arr[i]!=-1)
    	        printf("%d ",arr[i]);
        }
    }

---
+++ Remove_duplicates_from_an_array__.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j,c=0,k=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+	     for(j=0;j<n;j++)
+	   	{
+	       if(arr[i]==arr[j]&&i!=j)
+	       {
+	          arr[j]=-1;
+	       }
+	    }
+	    if(arr[i]!=-1)
+	        printf("%d ",arr[i]);
+    }
+}
