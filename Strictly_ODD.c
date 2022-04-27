531bb1afc601d4eb817bdd42eade94acd9be81f1
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-04-27 21:30:12 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,j=0,t=0,p=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {             if(a[i]%2==1)             {                 if(i%2==1)                 {                     p++;                 }             j++;             }             else if(a[i]%2==1)             {                 t++;             }     }     //printf("%d %d %d ",p,j,t);     if(t==0&&p==j)         printf("True");     else         printf("False"); }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,34 +1,32 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-   int i,n,r=0;
-   scanf("%d",&n);
-   int a[n];
-   for(i=0;i<n;i++)
-   {
-        scanf("%d",&a[i]);  
-   }
-   for(i=0;i<n;i++)
-   {
-       if(a[i]%2!=0)
-       {
-           if(i%2!=0)
-           {
-             r++;
-           }
-           else
-           {
-               r=0;
-               break;
-           }
-       }
-   }
-   if(r!=0)
-   {
-       printf("True");
-   }
-   else
-   {
-       printf("False");
-   }
+    int n,i,j=0,t=0,p=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+            if(a[i]%2==1)
+            {
+                if(i%2==1)
+                {
+                    p++;
+                }
+            j++;
+            }
+            else if(a[i]%2==1)
+            {
+                t++;
+            }
+    }
+    //printf("%d %d %d ",p,j,t);
+    if(t==0&&p==j)
+        printf("True");
+    else
+        printf("False");
 }
