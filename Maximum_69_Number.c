258675c385ff018a9e61101cbde6aade2f049228
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-09 20:36:30 +0530

    #include<stdio.h> int main() {     int a,r=0,b,rev=0,c,d=0;     scanf("%d",&a);     while(a!=0)     {         b=a%10;         a=a/10;         r=r*10+b;     }     while(r!=0)     {         c=r%10;         r=r/10;         if(d<1)         {             if(c==6)             {                 c=9;                 d=1;             }         }         rev=rev*10+c;     }     printf("%d",rev); }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,48 +1,27 @@
 #include<stdio.h>
-int rev(int a)
-{
-	int temp,rev=0,k=a;
-	while(k!=0)
-	{
-		temp=k%10;
-		 rev=(rev*10)+temp;
-		 k=k/10;
-	}
-	return rev;
-}
-int change(int n)
-{
-	int temp,i,re=0,k=n;
-	int arr[100],count=0;
-	while(k!=0)
-	{
-		temp=k%10;
-		 arr[count]=temp;
-		 k=k/10;
-		 count++;
-	}
-	for(i=0;i<count;i++)
-	{
-		if(arr[i]==6)
-		{
-			arr[i]=9;
-			break;
-		}
-	}
-	int p=0;
-	while(count--)
-	{
-		re=(re*10)+arr[p];
-		p++;
-	}
-	return re;
-}
 int main()
 {
-	int n,r,c;
-	scanf("%d",&n);
-	r= rev(n);
-	c= change(r);
-	printf("%d
-",c);
+    int a,r=0,b,rev=0,c,d=0;
+    scanf("%d",&a);
+    while(a!=0)
+    {
+        b=a%10;
+        a=a/10;
+        r=r*10+b;
+    }
+    while(r!=0)
+    {
+        c=r%10;
+        r=r/10;
+        if(d<1)
+        {
+            if(c==6)
+            {
+                c=9;
+                d=1;
+            }
+        }
+        rev=rev*10+c;
+    }
+    printf("%d",rev);
 }
